7e0eb5c238aab80dad78af6532ab7a2ec5dae004
Austin D <damek.austin28@svvsd.org>
2019-10-24 17:24:55 -0600

    Added lulu.js, the surface bridge between the surface arduino and submarine pi

---
+++ Software/Lulu/Pi/lulu.js
@@ -0,0 +1,27 @@
+var net = require('net');
+var HOST = '192.168.1.5';
+var PORT = 45246;
+var client = new net.Socket();
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+port.on("open", () => {
+  console.log('serial port open');
+});
+
+client.connect(PORT, HOST, function() {
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from t$
+    parser.on('data', data =>{
+       console.log("worked")
+       client.write(data);
+    });
+});
+
+client.on('error', function(err) {
+        console.log('Youve messed up: ' + err);
+});
+
