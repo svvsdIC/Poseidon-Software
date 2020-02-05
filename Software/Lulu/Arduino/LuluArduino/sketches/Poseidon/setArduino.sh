d721091e7a06be3051782bff9071126e2363e6c9
b0be <Bob_Ellis@HighMeadowDesigns.com>
2020-02-05 10:43:04 -0700

    Add shell scripts for easily setting up for Arduino command line

---
+++ Software/Lulu/Arduino/LuluArduino/sketches/Poseidon/setArduino.sh
@@ -0,0 +1,2 @@
+arduino --save-prefs --board arduino:avr:uno --pref sketchbook.path=/home/pi/Poseidon/Software/Lulu/Arduino/LuluArduino/sketches --pref serial.port=/dev/ttyACM0 --pref serial.port.file=ttyACM0
+echo "arduino set for arduino-uno and this sketchbook directory"
