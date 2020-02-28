22caba9bd871e660c7985c895e508e330c8ae1cb
UncertainQubit <39228494+UncertainQubit@users.noreply.github.com>
2020-02-27 17:11:38 -0700

    Modified CTempertaureSensor.cpp file to match .h file

--- Software/Nemo/Arduino/NemoArduino/sketches/Poseidon/CTemperatureSensor.cpp
+++ Software/Nemo/Arduino/NemoArduino/sketches/Poseidon/CTemperatureSensor.cpp
@@ -1,39 +1,40 @@
 #include "SysConfig.h"
 #if(HAS_SOME_MODULE)
-#include "CSome_module.h"
+#include <Arduino.h>
+#include "CTemperatureSensor.h"
 #include "NCommManager.h"
 
 
-CSome_module::CSome_module()
+CTemperatureSensor::CTemperatureSensor()
 {
 }
 
-void CSome_module::Initialize()
+void CTemperatureSensor::Initialize()
 {
-	Serial.println( F( "Some_module_init:1;" ) );
+	Serial.println( F( "TemperatureSensor_init:1;" ) );
 	m_statusTimer.Reset();
 }
 
-void CSome_module::Update( CCommand& commandIn )
+void CTemperatureSensor::Update( CCommand& commandIn )
 {
 	constexpr uint32_t kStatusDelay_ms	= 1000;							// 1hz
 
-    
+
 	// Handle commands
 	if( NCommManager::m_isCommandAvailable )
 	{
-		if( commandIn.Equals( "some_mod:cmd" ) )
+		if( commandIn.Equals( "TemperatureSensor:cmd" ) )
 		{
-			Serial.println( F( "some_mod:cmd,1;" ) );
+			Serial.println( F( "TemperatureSensor:cmd,1;" ) );
 		}
 	}
 
-    // Do other stuff 
+    // Do other stuff
 	if( m_statusTimer.HasElapsed( kStatusDelay_ms ) )
 	{
 		{
 			// Report results
-			Serial.print( F( "some_mod_cntr:" ) );Serial.println(++m_counter);
+			Serial.print( F( "TemperatureSensor_cntr:" ) );Serial.println(++m_counter);
 		}
 	}
 }
