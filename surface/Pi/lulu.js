789c6117b9bd4afd30c7ed29b9957e11f08f2790
b0be <Bob_Ellis@HighMeadowDesigns.com>
2020-09-03 11:28:32 -0600

    use correct port for arduino-uno

--- surface/Pi/lulu.js
+++ surface/Pi/lulu.js
@@ -1,11 +1,11 @@
 var net = require('net');
-var HOST = '192.168.1.5';
+var HOST = '192.168.1.8';
 var PORT = 45246;
 var client = new net.Socket();
 
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200 });
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 115200 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 
 port.on("open", () => {
@@ -16,7 +16,7 @@ client.connect(PORT, HOST, function() {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from t$
     parser.on('data', data =>{
-       console.log("worked")
+       console.log("passing thru: " + data);
        client.write(data);
     });
 });
