f2d13642569c7831de7fd078e3090d6a317bb8b3
b0be <Bob_Ellis@HighMeadowDesigns.com>
2020-09-03 10:44:58 -0600

    change path to new directory structure

--- rover/Arduino/sketches/Poseidon/setArduino.sh
+++ rover/Arduino/sketches/Poseidon/setArduino.sh
@@ -1,2 +1,2 @@
-arduino --save-prefs --board arduino:avr:mega:cpu=atmega2560 --pref sketchbook.path=/home/pi/Poseidon/Software/Nemo/Arduino/NemoArduino/sketches --pref serial.port=/dev/ttyACM0 --pref serial.port.file=ttyACM0
+arduino --save-prefs --board arduino:avr:mega:cpu=atmega2560 --pref sketchbook.path=/home/pi/Poseidon/rover/Arduino//sketches --pref serial.port=/dev/ttyACM0 --pref serial.port.file=ttyACM0
 echo "arduino set for mega2560 and this sketchbook directory"
