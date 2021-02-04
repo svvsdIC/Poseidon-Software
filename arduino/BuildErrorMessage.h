3d2509c9e69fea8e1c05f763ec24f36d8ec5875d
pablo maurin <maurin@gmail.com>
2021-02-03 20:45:41 -0700

    Provide a useful error message when build target is not specified

--- arduino/BuildErrorMessage.h
+++ arduino/BuildErrorMessage.h
@@ -3,4 +3,10 @@ This will always generate a compile time error message from the pre-processor.
 It is okay to include more than once.  Hence, no include guards
 */
 
-#error No build flag defined
+#error The build target must be defined at compile time. \
+       Please define the BUILD_ROVER or BUILD_SURFACE \
+       preprocessor macros.  Thise can be accomplished from the \
+       arduino-cli command line using the --build-property \
+       command line argument.  A sample build command would \
+       look something like this \
+           $ arduino-cli compile --fqbn arduino:avr:mega --build-property compiler.cpp.extra_flags=-DBUILD_ROVER --libraries libraries/
